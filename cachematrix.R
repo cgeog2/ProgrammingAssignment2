6502b96b025324673d26a17c8384d904802675ec
cgeog2 <cgeog2@yahoo.com>
2014-07-27 14:12:42 +0100

    Updated Solution Assignment 2
    This is my submission for Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions are used to save time and computational resources by using cached values if possible.
+## This is really useful if you are running a computation (like mean) on a long vector, or if it is done repeatedly in a loop.
+## Using cached values speeds up the process if the contecnts of the vector aren't changing.
 
-## Write a short comment describing this function
+## This function sets the value of this command, which is a matrix rather than a vector. 
+## I then ask it to get the value of that matrix using 'solve' to replace the 'mean' given in the example.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function looks to see if the mean has already been calculated in the first function.
+## If it is has it doesn't calculate it again but uses the cached value instead.
+## If it hasn't been done before, it calculates the mean and adds the value to the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
